7c724fd144e52bf0bcc830268cbf3c66d8d95b65
George Merington <georgemer1900@gmail.com>
2025-04-25 00:16:43 -0700

    ws setup

--- edocebiv/asgi.py
+++ edocebiv/asgi.py
@@ -11,12 +11,17 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import users.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edocebiv.settings')
 
+# Call setup first
+django_asgi_app = get_asgi_application()
+
+# Now it's safe to import your routing (models will load fine)
+import users.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             users.routing.websocket_urlpatterns
