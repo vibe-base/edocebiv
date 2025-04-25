b793f04ddfbdee23d08a03fff4de4323d6e7548d
George Merington <georgemer1900@gmail.com>
2025-04-25 00:20:50 -0700

    ws server setup

--- edocebiv/asgi.py
+++ edocebiv/asgi.py
@@ -11,6 +11,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edocebiv.settings')
 
@@ -22,9 +23,11 @@ import users.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            users.routing.websocket_urlpatterns
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                users.routing.websocket_urlpatterns
+            )
         )
     ),
 })
